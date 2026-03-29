3e651a5823019603442978bf9816a8163f657b7c
Ash Madhu <95035496+RisottoCurse@users.noreply.github.com>
2026-03-29 15:09:46 +0100

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        if (prices.empty()) {
+            return 0;
+        }
+
+        int l{0};
+        int r{1};
+        int maxProfit{0};
+
+        while (r < prices.size()) {
+            if (prices[r] > prices [l]) {
+                int profit = prices[r] - prices[l];
+                maxProfit = max(maxProfit, profit);   
+            }
+            else {
+                l = r;
+            }
+            r++;
+        }
+        return maxProfit;
+    }
+};
